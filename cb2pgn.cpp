5d9c336244d8d4b7b4f60af39e762a5c31b79695
Timm Knape <timm@knp.de>
2020-04-22 11:23:13 +0200

    move pawns

--- cb2pgn.cpp
+++ cb2pgn.cpp
@@ -79,11 +79,11 @@
 #line 228 "index.md"
 
 	void move(unsigned char &pos, int dx, int dy) {
-		int x = (pos & 0xf) + dx;
+		int x = (pos >> 4) + dx;
 		while (x > 8) { x -= 8; }
-		int y = (pos >> 4) + dy;
+		int y = (pos & 0xf) + dy;
 		while (y > 8) { y -= 8; }
-		pos = (y << 4) | x;
+		pos = (x << 4) | y;
 	}
 
 #line 240 "index.md"
@@ -91,32 +91,68 @@
 	void move_king(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no king to move"); }
 		std::cout << "K";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
 
-#line 252 "index.md"
+#line 253 "index.md"
 
 	void move_queen(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no queen to move"); }
 		std::cout << "Q";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
 
-#line 264 "index.md"
+#line 266 "index.md"
 
 	void move_rook(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no rook to move"); }
 		std::cout << "R";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
 
-#line 276 "index.md"
+#line 279 "index.md"
+
+	void move_bishop(unsigned char &pos, int dx, int dy) {
+		if (! pos) { fail("no bishop to move"); }
+		std::cout << "B";
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+
+#line 292 "index.md"
+
+	void move_knight(unsigned char &pos, int dx, int dy) {
+		if (! pos) { fail("no knight to move"); }
+		std::cout << "N";
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+
+#line 305 "index.md"
+
+	void move_pawn(unsigned char &pos, int dx, int dy, bool is_white) {
+		if (! pos) { fail("no pawn to move"); }
+		if (! is_white) { dy = 8 - dy; dx = (8 - dx) % 8; }
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+
+#line 318 "index.md"
 
 	void kingside_rochade(unsigned char &king, unsigned char *rooks) {
 		unsigned char *rook { nullptr };
@@ -135,7 +171,7 @@
 		std::cout << "O-O ";
 	}
 
-#line 297 "index.md"
+#line 339 "index.md"
 
 	void queenside_rochade(unsigned char &king, unsigned char *rooks) {
 		unsigned char *rook { nullptr };
@@ -251,7 +287,7 @@
 #line 196 "index.md"
 
 	
-#line 318 "index.md"
+#line 360 "index.md"
  {
 	if (! game_file.seekg(get_int(main_entry + 1), std::ifstream::beg)) {
 		fail("can't move to game at " + to_str(get_int(main_entry + 1)));
@@ -268,6 +304,11 @@
 	unsigned char b_rooks[] = { 0x18, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	unsigned char w_queens[] = { 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	unsigned char b_queens[] = { 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char w_bishops[] = { 0x31, 0x61, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char b_bishops[] = { 0x38, 0x68, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char w_knights[] = { 0x21, 0x71, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char b_knights[] = { 0x28, 0x78, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
 	bool white_is_playing { true };
 	if (len & 0x40000000) {
 		len &= 0x3fffffff;
@@ -284,16 +325,58 @@
 		auto &pawns { white_is_playing ? w_pawns : b_pawns };
 		auto &rooks { white_is_playing ? w_rooks : b_rooks };
 		auto &queens { white_is_playing ? w_queens : b_queens };
+		auto &bishops { white_is_playing ? w_bishops : b_bishops };
+		auto &knights { white_is_playing ? w_knights : b_knights };
+
 		int ch = (game_file.get() - count) & 0xff;
+		// std::cout << '{' << std::hex << (int) ch << std::dec << "} ";
 		switch (ch) {
+			case 0x02:
+				move_bishop(bishops[0], 1, 1);
+				break;
 			case 0x05:
 				move_rook(rooks[1], 2, 0);
+				break;
+			case 0x06:
+				move_bishop(bishops[0], 1, 7);
+				break;
+			case 0x07:
+				move_knight(knights[1], 7, 6);
+				break;
+			case 0x08:
+				move_bishop(bishops[1], 3, 3);
+				break;
+			case 0x09:
+				move_pawn(pawns[5], 0, 1, white_is_playing);
+				break;
+			case 0x0b:
+				move_pawn(pawns[3], 0, 2, white_is_playing);
+				break;
+			case 0x0e:
+				move_knight(knights[1], 1, 2);
+				break;
+			case 0x12:
+				move_pawn(pawns[7], 0, 1, white_is_playing);
+				break;
+			case 0x13:
+				move_pawn(pawns[7], 1, 1, white_is_playing);
 				break;
 			case 0x14:
 				move_rook(rooks[1], 0, 1);
 				break;
+			case 0x15:
+				move_pawn(pawns[4], 1, 1, white_is_playing);
+			case 0x16:
+				move_bishop(bishops[1], 7, 1);
+				break;
+			case 0x17:
+				move_pawn(pawns[1], 0, 2, white_is_playing);
+				break;
 			case 0x18:
 				move_queen(queens[0], 7, 1);
+				break;
+			case 0x19:
+				move_pawn(pawns[7], 7, 1, white_is_playing);
 				break;
 			case 0x21:
 				move_queen(queens[0], 4, 0);
@@ -307,6 +390,12 @@
 			case 0x28:
 				move_queen(queens[0], 3, 5);
 				break;
+			case 0x2c:
+				move_bishop(bishops[0], 5, 3);
+				break;
+			case 0x2d:
+				move_pawn(pawns[0], 0, 1, white_is_playing);
+				break;
 			case 0x2e:
 				move_rook(rooks[0], 1, 0);
 				break;
@@ -319,8 +408,35 @@
 			case 0x32:
 				move_rook(rooks[1], 6, 0);
 				break;
+			case 0x33:
+				move_pawn(pawns[7], 0, 2, white_is_playing);
+				break;
+			case 0x34:
+				move_knight(knights[1], 2, 7);
+				break;
+			case 0x35:
+				move_bishop(bishops[1], 1, 7);
+				break;
+			case 0x36:
+				move_pawn(pawns[4], 7, 1, white_is_playing);
+				break;
+			case 0x37:
+				move_bishop(bishops[0], 7, 1);
+				break;
 			case 0x39:
 				move_king(king, 1, 1);
+				break;
+			case 0x3a:
+				move_pawn(pawns[6], 7, 1, white_is_playing);
+				break;
+			case 0x3d:
+				move_knight(knights[0], 1, 2);
+				break;
+			case 0x3f:
+				move_bishop(bishops[1], 2, 2);
+				break;
+			case 0x41:
+				move_bishop(bishops[0], 4, 4);
 				break;
 			case 0x43:
 				move_rook(rooks[0], 0, 3);
@@ -334,6 +450,9 @@
 			case 0x49:
 				move_king(king, 0, 1);
 				break;
+			case 0x4a:
+				move_knight(knights[0], 2, 7);
+				break;
 			case 0x4d:
 				move_queen(queens[0], 1, 1);
 				break;
@@ -346,14 +465,26 @@
 			case 0x53:
 				move_queen(queens[0], 0, 4);
 				break;
+			case 0x55:
+				move_bishop(bishops[0], 3, 5);
+				break;
 			case 0x57:
 				move_queen(queens[0], 7, 0);
+				break;
+			case 0x58:
+				move_knight(knights[0], 2, 1);
 				break;
 			case 0x5a:
 				move_queen(queens[0], 6, 2);
 				break;
 			case 0x5d:
 				move_king(king, 1, 7);
+				break;
+			case 0x5e:
+				move_bishop(bishops[1], 6, 6);
+				break;
+			case 0x5f:
+				move_knight(knights[1], 6, 1);
 				break;
 			case 0x61:
 				move_rook(rooks[0], 6, 0);
@@ -364,11 +495,17 @@
 			case 0x63:
 				move_rook(rooks[0], 0, 5);
 				break;
+			case 0x64:
+				move_pawn(pawns[1], 0, 1, white_is_playing);
+				break;
 			case 0x68:
 				move_rook(rooks[1], 0, 3);
 				break;
 			case 0x6b:
 				move_queen(queens[0], 0, 6);
+				break;
+			case 0x6d:
+				move_bishop(bishops[1], 3, 5);
 				break;
 			case 0x6e:
 				move_queen(queens[0], 4, 4);
@@ -376,20 +513,53 @@
 			case 0x6f:
 				move_rook(rooks[0], 7, 0);
 				break;
+			case 0x70:
+				move_pawn(pawns[1], 1, 1, white_is_playing);
+				break;
+			case 0x71:
+				move_bishop(bishops[1], 4, 4);
+				break;
+			case 0x73:
+				move_bishop(bishops[1], 5, 5);
+				break;
+			case 0x75:
+				move_knight(knights[1], 6, 7);
+				break;
 			case 0x76:
 				kingside_rochade(king, rooks);
 				break;
 			case 0x77:
 				move_rook(rooks[1], 0, 6);
 				break;
+			case 0x78:
+				move_bishop(bishops[1], 7, 7);
+				break;
 			case 0x79:
 				move_queen(queens[0], 1, 0);
+				break;
+			case 0x7b:
+				move_pawn(pawns[2], 0, 1, white_is_playing);
+				break;
+			case 0x7c:
+				move_bishop(bishops[0], 6, 6);
+				break;
+			case 0x7d:
+				move_pawn(pawns[5], 1, 1, white_is_playing);
 				break;
 			case 0x7f:
 				move_queen(queens[0], 0, 5);
 				break;
+			case 0x84:
+				move_pawn(pawns[4], 0, 1, white_is_playing);
+				break;
+			case 0x85:
+				move_pawn(pawns[2], 7, 1, white_is_playing);
+				break;
 			case 0x88:
 				move_rook(rooks[0], 4, 0);
+				break;
+			case 0x89:
+				move_knight(knights[1], 1, 6);
 				break;
 			case 0x8b:
 				move_rook(rooks[1], 3, 0);
@@ -397,8 +567,20 @@
 			case 0x8d:
 				move_queen(queens[0], 0, 7);
 				break;
+			case 0x8e:
+				move_pawn(pawns[0], 1, 1, white_is_playing);
+				break;
+			case 0x90:
+				move_pawn(pawns[3], 1, 1, white_is_playing);
+				break;
+			case 0x93:
+				move_bishop(bishops[1], 4, 4);
+				break;
 			case 0x96:
 				move_queen(queens[0], 7, 7);
+				break;
+			case 0x97:
+				move_bishop(bishops[0], 2, 2);
 				break;
 			case 0x98:
 				move_rook(rooks[1], 5, 0);
@@ -409,8 +591,17 @@
 			case 0x9c:
 				move_queen(rooks[0], 0, 6);
 				break;
+			case 0x9e:
+				move_pawn(pawns[5], 0, 2, white_is_playing);
+				break;
 			case 0xa1:
 				move_rook(rooks[1], 4, 0);
+				break;
+			case 0xa2:
+				move_bishop(bishops[1], 5, 3);
+				break;
+			case 0xa4:
+				move_pawn(pawns[1], 7, 1, white_is_playing);
 				break;
 			case 0xa5:
 				move_queen(queens[0], 0, 1);
@@ -424,6 +615,9 @@
 			case 0xa9:
 				move_rook(rooks[1], 0, 2);
 				break;
+			case 0xae:
+				move_bishop(bishops[0], 6, 2);
+				break;
 			case 0xb1:
 				move_king(king, 7, 7);
 				break;
@@ -436,8 +630,23 @@
 			case 0xb5:
 				queenside_rochade(king, rooks);
 				break;
+			case 0xb7:
+				move_bishop(bishops[0], 2, 6);
+				break;
 			case 0xb8:
 				move_queen(queens[0], 0, 2);
+				break;
+			case 0xd9:
+				move_bishop(bishops[0], 4, 4);
+				break;
+			case 0xba:
+				move_knight(knights[0], 6, 7);
+				break;
+			case 0xbb:
+				move_pawn(pawns[6], 0, 1, white_is_playing);
+				break;
+			case 0xbc:
+				move_pawn(pawns[6], 1, 1, white_is_playing);
 				break;
 			case 0xbd:
 				move_queen(queens[0], 5, 5);
@@ -448,8 +657,20 @@
 			case 0xbf:
 				move_queen(queens[0], 3, 3);
 				break;
+			case 0xc1:
+				move_pawn(pawns[0], 0, 2, white_is_playing);
+				break;
 			case 0xc2:
 				move_king(king, 0, 7);
+				break;
+			case 0xc3:
+				move_bishop(bishops[0], 5, 5);
+				break;
+			case 0xc4:
+				move_knight(knights[1], 2, 1);
+				break;
+			case 0xc5:
+				move_pawn(pawns[3], 0, 1, white_is_playing);
 				break;
 			case 0xc6:
 				move_rook(rooks[0], 2, 0);
@@ -460,17 +681,44 @@
 			case 0xd2:
 				move_queen(queens[0], 0, 6);
 				break;
+			case 0xd4:
+				move_knight(knights[0], 7, 6);
+				break;
 			case 0xd7:
 				move_rook(rooks[0], 0, 4);
 				break;
 			case 0xd8:
 				move_king(king, 1, 0);
 				break;
+			case 0xde:
+				move_pawn(pawns[5], 7, 1, white_is_playing);
+				break;
+			case 0xda:
+				move_pawn(pawns[2], 0, 2, white_is_playing);
+				break;
+			case 0xdd:
+				move_knight(knights[0], 1, 6);
+				break;
+			case 0xdf:
+				move_pawn(pawns[6], 0, 2, white_is_playing);
+				break;
+			case 0xe0:
+				move_pawn(pawns[2], 1, 1, white_is_playing);
+				break;
+			case 0xe1:
+				move_bishop(bishops[0], 3, 3);
+				break;
 			case 0xe2:
 				move_rook(rooks[1], 0, 7);
 				break;
+			case 0xe4:
+				move_bishop(bishops[0], 7, 7);
+				break;
 			case 0xe6:
 				move_rook(rooks[0], 0, 7);
+				break;
+			case 0xe9:
+				move_knight(knights[0], 6, 1);
 				break;
 			case 0xeb:
 				move_queen(queens[0], 3, 0);
@@ -478,11 +726,35 @@
 			case 0xee:
 				move_rook(rooks[1], 0, 4);
 				break;
+			case 0xf2:
+				move_bishop(bishops[1], 2, 6);
+				break;
+			case 0xf3:
+				move_bishop(bishops[1], 6, 2);
+				break;
+			case 0xf5:
+				move_pawn(pawns[0], 7, 1, white_is_playing);
+				break;
+			case 0xf6:
+				move_bishop(bishops[1], 1, 1);
+				break;
 			case 0xf8:
 				move_rook(rooks[0], 0, 2);
 				break;
+			case 0xf9:
+				move_pawn(pawns[3], 7, 1, white_is_playing);
+				break;
+			case 0xfa:
+				move_knight(knights[0], 7, 2);
+				break;
 			case 0xfb:
 				move_rook(rooks[1], 0, 5);
+				break;
+			case 0xfe:
+				move_knight(knights[1], 7, 2);
+				break;
+			case 0xff:
+				move_pawn(pawns[4], 0, 2, white_is_playing);
 				break;
 			default:
 				std::cout << "xx ";
--- index.md
+++ index.md
@@ -227,11 +227,11 @@
 ```
 @add(globals)
 	void move(unsigned char &pos, int dx, int dy) {
-		int x = (pos & 0xf) + dx;
+		int x = (pos >> 4) + dx;
 		while (x > 8) { x -= 8; }
-		int y = (pos >> 4) + dy;
+		int y = (pos & 0xf) + dy;
 		while (y > 8) { y -= 8; }
-		pos = (y << 4) | x;
+		pos = (x << 4) | y;
 	}
 @end(globals)
 ```
@@ -241,7 +241,8 @@
 	void move_king(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no king to move"); }
 		std::cout << "K";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
@@ -253,7 +254,8 @@
 	void move_queen(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no queen to move"); }
 		std::cout << "Q";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
@@ -265,7 +267,47 @@
 	void move_rook(unsigned char &pos, int dx, int dy) {
 		if (! pos) { fail("no rook to move"); }
 		std::cout << "R";
-		move (pos, dx, dy);
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+@end(globals)
+```
+
+```
+@add(globals)
+	void move_bishop(unsigned char &pos, int dx, int dy) {
+		if (! pos) { fail("no bishop to move"); }
+		std::cout << "B";
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+@end(globals)
+```
+
+```
+@add(globals)
+	void move_knight(unsigned char &pos, int dx, int dy) {
+		if (! pos) { fail("no knight to move"); }
+		std::cout << "N";
+		out_pos(pos);
+		move(pos, dx, dy);
+		out_pos(pos);
+		std::cout << " ";
+	}
+@end(globals)
+```
+
+```
+@add(globals)
+	void move_pawn(unsigned char &pos, int dx, int dy, bool is_white) {
+		if (! pos) { fail("no pawn to move"); }
+		if (! is_white) { dy = 8 - dy; dx = (8 - dx) % 8; }
+		out_pos(pos);
+		move(pos, dx, dy);
 		out_pos(pos);
 		std::cout << " ";
 	}
@@ -331,6 +373,11 @@
 	unsigned char b_rooks[] = { 0x18, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	unsigned char w_queens[] = { 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	unsigned char b_queens[] = { 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char w_bishops[] = { 0x31, 0x61, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char b_bishops[] = { 0x38, 0x68, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char w_knights[] = { 0x21, 0x71, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	unsigned char b_knights[] = { 0x28, 0x78, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
 	bool white_is_playing { true };
 	if (len & 0x40000000) {
 		len &= 0x3fffffff;
@@ -347,16 +394,58 @@
 		auto &pawns { white_is_playing ? w_pawns : b_pawns };
 		auto &rooks { white_is_playing ? w_rooks : b_rooks };
 		auto &queens { white_is_playing ? w_queens : b_queens };
+		auto &bishops { white_is_playing ? w_bishops : b_bishops };
+		auto &knights { white_is_playing ? w_knights : b_knights };
+
 		int ch = (game_file.get() - count) & 0xff;
+		// std::cout << '{' << std::hex << (int) ch << std::dec << "} ";
 		switch (ch) {
+			case 0x02:
+				move_bishop(bishops[0], 1, 1);
+				break;
 			case 0x05:
 				move_rook(rooks[1], 2, 0);
+				break;
+			case 0x06:
+				move_bishop(bishops[0], 1, 7);
+				break;
+			case 0x07:
+				move_knight(knights[1], 7, 6);
+				break;
+			case 0x08:
+				move_bishop(bishops[1], 3, 3);
+				break;
+			case 0x09:
+				move_pawn(pawns[5], 0, 1, white_is_playing);
+				break;
+			case 0x0b:
+				move_pawn(pawns[3], 0, 2, white_is_playing);
+				break;
+			case 0x0e:
+				move_knight(knights[1], 1, 2);
+				break;
+			case 0x12:
+				move_pawn(pawns[7], 0, 1, white_is_playing);
+				break;
+			case 0x13:
+				move_pawn(pawns[7], 1, 1, white_is_playing);
 				break;
 			case 0x14:
 				move_rook(rooks[1], 0, 1);
 				break;
+			case 0x15:
+				move_pawn(pawns[4], 1, 1, white_is_playing);
+			case 0x16:
+				move_bishop(bishops[1], 7, 1);
+				break;
+			case 0x17:
+				move_pawn(pawns[1], 0, 2, white_is_playing);
+				break;
 			case 0x18:
 				move_queen(queens[0], 7, 1);
+				break;
+			case 0x19:
+				move_pawn(pawns[7], 7, 1, white_is_playing);
 				break;
 			case 0x21:
 				move_queen(queens[0], 4, 0);
@@ -370,6 +459,12 @@
 			case 0x28:
 				move_queen(queens[0], 3, 5);
 				break;
+			case 0x2c:
+				move_bishop(bishops[0], 5, 3);
+				break;
+			case 0x2d:
+				move_pawn(pawns[0], 0, 1, white_is_playing);
+				break;
 			case 0x2e:
 				move_rook(rooks[0], 1, 0);
 				break;
@@ -382,8 +477,35 @@
 			case 0x32:
 				move_rook(rooks[1], 6, 0);
 				break;
+			case 0x33:
+				move_pawn(pawns[7], 0, 2, white_is_playing);
+				break;
+			case 0x34:
+				move_knight(knights[1], 2, 7);
+				break;
+			case 0x35:
+				move_bishop(bishops[1], 1, 7);
+				break;
+			case 0x36:
+				move_pawn(pawns[4], 7, 1, white_is_playing);
+				break;
+			case 0x37:
+				move_bishop(bishops[0], 7, 1);
+				break;
 			case 0x39:
 				move_king(king, 1, 1);
+				break;
+			case 0x3a:
+				move_pawn(pawns[6], 7, 1, white_is_playing);
+				break;
+			case 0x3d:
+				move_knight(knights[0], 1, 2);
+				break;
+			case 0x3f:
+				move_bishop(bishops[1], 2, 2);
+				break;
+			case 0x41:
+				move_bishop(bishops[0], 4, 4);
 				break;
 			case 0x43:
 				move_rook(rooks[0], 0, 3);
@@ -397,6 +519,9 @@
 			case 0x49:
 				move_king(king, 0, 1);
 				break;
+			case 0x4a:
+				move_knight(knights[0], 2, 7);
+				break;
 			case 0x4d:
 				move_queen(queens[0], 1, 1);
 				break;
@@ -409,14 +534,26 @@
 			case 0x53:
 				move_queen(queens[0], 0, 4);
 				break;
+			case 0x55:
+				move_bishop(bishops[0], 3, 5);
+				break;
 			case 0x57:
 				move_queen(queens[0], 7, 0);
+				break;
+			case 0x58:
+				move_knight(knights[0], 2, 1);
 				break;
 			case 0x5a:
 				move_queen(queens[0], 6, 2);
 				break;
 			case 0x5d:
 				move_king(king, 1, 7);
+				break;
+			case 0x5e:
+				move_bishop(bishops[1], 6, 6);
+				break;
+			case 0x5f:
+				move_knight(knights[1], 6, 1);
 				break;
 			case 0x61:
 				move_rook(rooks[0], 6, 0);
@@ -427,11 +564,17 @@
 			case 0x63:
 				move_rook(rooks[0], 0, 5);
 				break;
+			case 0x64:
+				move_pawn(pawns[1], 0, 1, white_is_playing);
+				break;
 			case 0x68:
 				move_rook(rooks[1], 0, 3);
 				break;
 			case 0x6b:
 				move_queen(queens[0], 0, 6);
+				break;
+			case 0x6d:
+				move_bishop(bishops[1], 3, 5);
 				break;
 			case 0x6e:
 				move_queen(queens[0], 4, 4);
@@ -439,20 +582,53 @@
 			case 0x6f:
 				move_rook(rooks[0], 7, 0);
 				break;
+			case 0x70:
+				move_pawn(pawns[1], 1, 1, white_is_playing);
+				break;
+			case 0x71:
+				move_bishop(bishops[1], 4, 4);
+				break;
+			case 0x73:
+				move_bishop(bishops[1], 5, 5);
+				break;
+			case 0x75:
+				move_knight(knights[1], 6, 7);
+				break;
 			case 0x76:
 				kingside_rochade(king, rooks);
 				break;
 			case 0x77:
 				move_rook(rooks[1], 0, 6);
 				break;
+			case 0x78:
+				move_bishop(bishops[1], 7, 7);
+				break;
 			case 0x79:
 				move_queen(queens[0], 1, 0);
+				break;
+			case 0x7b:
+				move_pawn(pawns[2], 0, 1, white_is_playing);
+				break;
+			case 0x7c:
+				move_bishop(bishops[0], 6, 6);
+				break;
+			case 0x7d:
+				move_pawn(pawns[5], 1, 1, white_is_playing);
 				break;
 			case 0x7f:
 				move_queen(queens[0], 0, 5);
 				break;
+			case 0x84:
+				move_pawn(pawns[4], 0, 1, white_is_playing);
+				break;
+			case 0x85:
+				move_pawn(pawns[2], 7, 1, white_is_playing);
+				break;
 			case 0x88:
 				move_rook(rooks[0], 4, 0);
+				break;
+			case 0x89:
+				move_knight(knights[1], 1, 6);
 				break;
 			case 0x8b:
 				move_rook(rooks[1], 3, 0);
@@ -460,8 +636,20 @@
 			case 0x8d:
 				move_queen(queens[0], 0, 7);
 				break;
+			case 0x8e:
+				move_pawn(pawns[0], 1, 1, white_is_playing);
+				break;
+			case 0x90:
+				move_pawn(pawns[3], 1, 1, white_is_playing);
+				break;
+			case 0x93:
+				move_bishop(bishops[1], 4, 4);
+				break;
 			case 0x96:
 				move_queen(queens[0], 7, 7);
+				break;
+			case 0x97:
+				move_bishop(bishops[0], 2, 2);
 				break;
 			case 0x98:
 				move_rook(rooks[1], 5, 0);
@@ -472,8 +660,17 @@
 			case 0x9c:
 				move_queen(rooks[0], 0, 6);
 				break;
+			case 0x9e:
+				move_pawn(pawns[5], 0, 2, white_is_playing);
+				break;
 			case 0xa1:
 				move_rook(rooks[1], 4, 0);
+				break;
+			case 0xa2:
+				move_bishop(bishops[1], 5, 3);
+				break;
+			case 0xa4:
+				move_pawn(pawns[1], 7, 1, white_is_playing);
 				break;
 			case 0xa5:
 				move_queen(queens[0], 0, 1);
@@ -487,6 +684,9 @@
 			case 0xa9:
 				move_rook(rooks[1], 0, 2);
 				break;
+			case 0xae:
+				move_bishop(bishops[0], 6, 2);
+				break;
 			case 0xb1:
 				move_king(king, 7, 7);
 				break;
@@ -499,8 +699,23 @@
 			case 0xb5:
 				queenside_rochade(king, rooks);
 				break;
+			case 0xb7:
+				move_bishop(bishops[0], 2, 6);
+				break;
 			case 0xb8:
 				move_queen(queens[0], 0, 2);
+				break;
+			case 0xd9:
+				move_bishop(bishops[0], 4, 4);
+				break;
+			case 0xba:
+				move_knight(knights[0], 6, 7);
+				break;
+			case 0xbb:
+				move_pawn(pawns[6], 0, 1, white_is_playing);
+				break;
+			case 0xbc:
+				move_pawn(pawns[6], 1, 1, white_is_playing);
 				break;
 			case 0xbd:
 				move_queen(queens[0], 5, 5);
@@ -511,8 +726,20 @@
 			case 0xbf:
 				move_queen(queens[0], 3, 3);
 				break;
+			case 0xc1:
+				move_pawn(pawns[0], 0, 2, white_is_playing);
+				break;
 			case 0xc2:
 				move_king(king, 0, 7);
+				break;
+			case 0xc3:
+				move_bishop(bishops[0], 5, 5);
+				break;
+			case 0xc4:
+				move_knight(knights[1], 2, 1);
+				break;
+			case 0xc5:
+				move_pawn(pawns[3], 0, 1, white_is_playing);
 				break;
 			case 0xc6:
 				move_rook(rooks[0], 2, 0);
@@ -523,17 +750,44 @@
 			case 0xd2:
 				move_queen(queens[0], 0, 6);
 				break;
+			case 0xd4:
+				move_knight(knights[0], 7, 6);
+				break;
 			case 0xd7:
 				move_rook(rooks[0], 0, 4);
 				break;
 			case 0xd8:
 				move_king(king, 1, 0);
 				break;
+			case 0xde:
+				move_pawn(pawns[5], 7, 1, white_is_playing);
+				break;
+			case 0xda:
+				move_pawn(pawns[2], 0, 2, white_is_playing);
+				break;
+			case 0xdd:
+				move_knight(knights[0], 1, 6);
+				break;
+			case 0xdf:
+				move_pawn(pawns[6], 0, 2, white_is_playing);
+				break;
+			case 0xe0:
+				move_pawn(pawns[2], 1, 1, white_is_playing);
+				break;
+			case 0xe1:
+				move_bishop(bishops[0], 3, 3);
+				break;
 			case 0xe2:
 				move_rook(rooks[1], 0, 7);
 				break;
+			case 0xe4:
+				move_bishop(bishops[0], 7, 7);
+				break;
 			case 0xe6:
 				move_rook(rooks[0], 0, 7);
+				break;
+			case 0xe9:
+				move_knight(knights[0], 6, 1);
 				break;
 			case 0xeb:
 				move_queen(queens[0], 3, 0);
@@ -541,11 +795,35 @@
 			case 0xee:
 				move_rook(rooks[1], 0, 4);
 				break;
+			case 0xf2:
+				move_bishop(bishops[1], 2, 6);
+				break;
+			case 0xf3:
+				move_bishop(bishops[1], 6, 2);
+				break;
+			case 0xf5:
+				move_pawn(pawns[0], 7, 1, white_is_playing);
+				break;
+			case 0xf6:
+				move_bishop(bishops[1], 1, 1);
+				break;
 			case 0xf8:
 				move_rook(rooks[0], 0, 2);
 				break;
+			case 0xf9:
+				move_pawn(pawns[3], 7, 1, white_is_playing);
+				break;
+			case 0xfa:
+				move_knight(knights[0], 7, 2);
+				break;
 			case 0xfb:
 				move_rook(rooks[1], 0, 5);
+				break;
+			case 0xfe:
+				move_knight(knights[1], 7, 2);
+				break;
+			case 0xff:
+				move_pawn(pawns[4], 0, 2, white_is_playing);
 				break;
 			default:
 				std::cout << "xx ";
